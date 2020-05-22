22451ea46e4ed1e8a2a96d0045da56b20f24a05e
kimhaneui <kimhaneui2@naver.com>
2020-05-22 15:09:19 +0900

    getview 매소드 오버라이딩

--- app/src/main/java/com/example/bangproject_0522/adapters/RoomAdapter.java
+++ app/src/main/java/com/example/bangproject_0522/adapters/RoomAdapter.java
@@ -2,10 +2,14 @@ package com.example.bangproject_0522.adapters;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
+import com.example.bangproject_0522.R;
 import com.example.bangproject_0522.datas.Room;
 
 import java.util.List;
@@ -21,5 +25,15 @@ public class RoomAdapter extends ArrayAdapter<Room> {
         mContext = context;
         mList = objects;
         inf = LayoutInflater.from(mContext);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View row = convertView;
+        if (row==null){
+            row = inf.inflate(R.layout.room_list_item,null);
+        }
+        return row;
     }
 }
